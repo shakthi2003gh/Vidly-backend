7a67f38b7258fc976b234541c29f3cbe1e5bfe35
shakthi <shakthikumar123456789@gmail.com>
2023-02-10 12:02:04 +0530

    Unit test user jwt generate function

---
+++ tests/unit/models/user.test.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const { User } = require("../../../models/user");
+
+describe("user.generateAuthToken", () => {
+  it("should return a valid JWT", () => {
+    const _id = new mongoose.Types.ObjectId().toHexString();
+    const payload = { _id, isAdmin: true };
+
+    const user = new User(payload);
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+    expect(decoded).toMatchObject(payload);
+  });
+});
